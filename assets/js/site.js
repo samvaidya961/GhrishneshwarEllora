abd13f7ac19ab4b00819666922439489b4712050
samvaidya961 <67541221+samvaidya961@users.noreply.github.com>
2021-01-19 20:38:20 +0530

    Add files via upload

--- assets/js/site.js
+++ assets/js/site.js
@@ -7,3 +7,19 @@ function sidebar_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
 }
+
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+}
+
+function myFunction1(imgs) {
+    var expandImg = document.getElementById("expandedImg1");
+    var imgText = document.getElementById("imgtext1");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+}
